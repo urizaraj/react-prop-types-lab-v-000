b5b1464e2532bc998ee05815a6a28b1274d8e541
Alexander Urizar <urizaraj@gmail.com>
2018-04-13 16:42:24 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,15 @@
-// Code Product Component Here
+import React from 'react'
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.name}
+        {this.props.producer}
+        {this.props.hasWatermark}
+        {this.props.color}
+        {this.props.weight}
+      </div>
+    )
+  }
+}
